f4a46ccfda46ce72af9ffa1c8eff87620a7e3bc6
FrankYin <djmax96945147@outlook.com>
2023-11-12 03:52:56 +0100

    change the beat frequency

--- Assets/Scripts/Python/rosa.py
+++ Assets/Scripts/Python/rosa.py
@@ -8,21 +8,26 @@ app = Flask(__name__)
 def upload_file():
     if 'file' not in request.files:
         return "No file part"
-    
+
     file = request.files['file']
     if file.filename == '':
         return "No selected file"
-    
+
     if file:
         audio, sr = librosa.load(file, sr=None)
-        pitches, magnitudes = librosa.piptrack(y=audio, sr=sr)
-        
+
+        # Define the frame and hop length
+        frame_length = int(sr * 0.2)  # 0.2 seconds frame
+        hop_length = int(sr * 0.2)  # 0.2 seconds hop
+
+        pitches, magnitudes = librosa.piptrack(y=audio, sr=sr, n_fft=frame_length, hop_length=hop_length)
+
         # Extracting pitch values and their timestamps
         pitch_data = []
         for t in range(pitches.shape[1]):
             index = magnitudes[:, t].argmax()
             pitch = pitches[index, t]
-            time = librosa.frames_to_time(t, sr=sr)
+            time = librosa.frames_to_time(t, sr=sr, hop_length=hop_length)
             pitch_data.append({'time': time, 'pitch': float(pitch)})  # Convert pitch to float
 
         # TODO: Map pitch values to musical notes
